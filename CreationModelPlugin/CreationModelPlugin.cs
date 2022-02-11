5fabe2951c5cd0500d7ed4917231d896b04d5b23
xbepxx@gmail.com <xbepxx@gmail.com>
2022-02-11 17:42:18 +0300

    1

--- CreationModelPlugin/CreationModelPlugin.cs
+++ CreationModelPlugin/CreationModelPlugin.cs
@@ -91,16 +91,14 @@ namespace CreationModelPlugin
                 Wall wall = Wall.Create(doc, line, level1.Id, false);
                 walls.Add(wall);
                 wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
-                
-                //AddDoor(doc, level1, walls[0]);
-                AddRoof(doc, level2, walls);
-                //AddRoof2(doc, level2, walls);
             }
+            AddRoof2(doc, level2, walls, width, dept);
+            //AddRoof(doc, level2, walls);
             transaction.Commit();
             return Result.Succeeded;
         }
 
-        private void AddRoof2(Document doc, Level level2, List<Wall> walls)
+        private void AddRoof2(Document doc, Level level2, List<Wall> walls, double width, double dept)
         {
             RoofType roofType = new FilteredElementCollector(doc)
                 .OfClass(typeof(RoofType))
@@ -108,29 +106,30 @@ namespace CreationModelPlugin
                 .Where(x => x.Name.Equals("Типовой - 125мм"))
                 .Where(x => x.FamilyName.Equals("Базовая крыша"))
                 .FirstOrDefault();
+
+            View view = new FilteredElementCollector(doc) //получаем вид
+                .OfClass(typeof(View))
+                .OfType<View>()
+                .Where(x => x.Name.Equals("Уровень 2"))
+                .FirstOrDefault();
+
             //Добавим смещение крыши относительно оси стены
             double wallWidth = walls[0].Width;
             double dt = wallWidth / 2;
-            List<XYZ> points = new List<XYZ>(); //собираем в список
-            points.Add(new XYZ(-dt, -dt, 0));
-            points.Add(new XYZ(dt, -dt, 0));
-            points.Add(new XYZ(dt, dt, 0));
-            points.Add(new XYZ(-dt, dt, 0));
-            points.Add(new XYZ(-dt, -dt, 0));
 
-            Application application = doc.Application;
-            CurveArray curveArray = application.Create.NewCurveArray(); //создаём кривую
-            for (int i = 0; i < 4; i++)
-            {
-                LocationCurve curve = walls[i].Location as LocationCurve; //получаем кривые
-                //footprint.Append(curve.Curve); //добавляем с помощью метода Append..Curve получает кривую из LocationCurve. Метод строит до середины стены
-                XYZ p1 = curve.Curve.GetEndPoint(0);
-                XYZ p2 = curve.Curve.GetEndPoint(1);
-                Line line = Line.CreateBound(p1 + points[i], p2 + points[i + 1]);
-                curveArray.Append(line);
-            }
-            ReferencePlane plane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 20), new XYZ(0, 20, 0), doc.ActiveView);
-            doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, 0, 40);
+            double extrusionFirst = -width / 2 - dt; //выдавливание крыши. Начало с одной стороны
+            double extrusionEnd = width / 2 + dt; //выдавливание крыши. Конец с одной стороны
+
+            double curveFirst = -dept / 2 - dt; //начало кривой кровли
+            double curveEnd = dept / 2 + dt; //конец кривой кровли
+
+            CurveArray curveArray = new CurveArray(); //создаём кривую профиля кровли
+            curveArray.Append(Line.CreateBound(new XYZ(0, curveFirst, level2.Elevation), new XYZ(0, 0, level2.Elevation + 10))); //Elevation - базовая высота уровня. new XYZ -создаём новые точки
+            curveArray.Append(Line.CreateBound(new XYZ(0, 0, level2.Elevation + 10), new XYZ(0, curveEnd, level2.Elevation))); //Elevation - базовая высота уровня. new XYZ -создаём новые точки
+            ReferencePlane plane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 20), new XYZ(0, 20, 0), view);
+            ExtrusionRoof extrusionRoof= doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, extrusionFirst, extrusionEnd);
+            extrusionRoof.EaveCuts = EaveCutterType.TwoCutSquare; //задаём 2 ската
+
         }
 
         private void AddRoof(Document doc, Level level2, List<Wall> walls) //создаём крышу
@@ -176,11 +175,11 @@ namespace CreationModelPlugin
             //}
             #endregion
             #region Уклон крыши. Вариант 2
-            //foreach(ModelCurve m in footPrintToModelCurveMapping)
-            //{
-            //    footprintRoof.set_DefinesSlope(m, true);
-            //    footprintRoof.set_SlopeAngle(m, 0.5);
-            //}
+            foreach (ModelCurve m in footPrintToModelCurveMapping)
+            {
+                footprintRoof.set_DefinesSlope(m, true);
+                footprintRoof.set_SlopeAngle(m, 0.5);
+            }
             #endregion
         }
         private void AddDoor(Document doc, Level level1, Wall wall)
